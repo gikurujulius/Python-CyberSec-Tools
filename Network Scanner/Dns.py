ac5dd77f6ad132e1c4318f1d95bd11d08f533447
Julius Gikuru <108215936+gikurujulius@users.noreply.github.com>
2023-04-28 15:33:42 +0300

    Record Types Update

--- Network Scanner/Dns.py
+++ Network Scanner/Dns.py
@@ -16,15 +16,16 @@ def ReverseDNS(ip):
 
 def DnsRequest(target_domain):
     ips = []
-    try:
-        result = dns.resolver.resolve(target_domain)
-        if result:
-            print(target_domain)
-            for answer in result:
-                print(answer)
-                print("Domain Names: %s" % ReverseDNS(answer.to_text()))
-    except (dns.resolver.NXDOMAIN, dns.exception.Timeout):
-        return []
+    for record_type in record_types:
+        try:
+            result = dns.resolver.resolve(target_domain, record_type)
+            if result:
+                print(target_domain)
+                for answer in result:
+                    print(f"{record_type} records for {target_domain}: ")
+                    print("Domain Names: %s" % ReverseDNS(answer.to_text()))
+        except (dns.resolver.NXDOMAIN, dns.exception.Timeout, dns.resolver.NoAnswer):
+            return []
     return ips
        
 def Subdomain_Search(target_domain, dictionary, nums):
@@ -38,7 +39,7 @@ def Subdomain_Search(target_domain, dictionary, nums):
     
         
 target_domain = input("Enter your target: ")
-subdomain_files = ""
+subdomain_files = "subdomains-top1million-110000.txt"
 record_types = ["A", "AAAA", "CNAME", "MX", "NS", "SOA", "TXT"]
 dictionary = []
 with open(subdomain_files, "r") as f:
