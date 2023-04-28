a1f06f6d1feb31df9be15db8951b53b5f2996010
Julius Gikuru <108215936+gikurujulius@users.noreply.github.com>
2023-04-28 13:37:14 +0300

    Dns scanner

---
+++ Network Scanner/Dns.py
@@ -0,0 +1,47 @@
+
+import sys
+import socket
+import dns
+import dns.resolver
+
+        
+        
+def ReverseDNS(ip):
+    try:
+        result = socket.gethostbyaddr(ip)
+        return [result[0]]+result[1]
+    except socket.herror:
+        return None
+
+
+def DnsRequest(target_domain):
+    ips = []
+    try:
+        result = dns.resolver.resolve(target_domain)
+        if result:
+            print(target_domain)
+            for answer in result:
+                print(answer)
+                print("Domain Names: %s" % ReverseDNS(answer.to_text()))
+    except (dns.resolver.NXDOMAIN, dns.exception.Timeout):
+        return []
+    return ips
+       
+def Subdomain_Search(target_domain, dictionary, nums):
+    for word in dictionary:
+        subdomain = word+"." + target_domain
+        DnsRequest(subdomain)
+        if nums:
+            for i in range(0,10):
+                s = word + str(i) + "." + target_domain
+                DnsRequest(s)
+    
+        
+target_domain = input("Enter your target: ")
+subdomain_files = ""
+record_types = ["A", "AAAA", "CNAME", "MX", "NS", "SOA", "TXT"]
+dictionary = []
+with open(subdomain_files, "r") as f:
+    dictionary = f.read().splitlines()
+    
+Subdomain_Search(target_domain, dictionary, True)
